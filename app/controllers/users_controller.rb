3776bc9f6a864ade345c84ca9d45a1b5ef1c4cc1
EC2 Default User <ec2-user@ip-172-31-28-159.ec2.internal>
2021-12-09 10:34:00 +0000

    非同期通信実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,7 @@ class UsersController < ApplicationController
   
   def edit
     @user = User.find(params[:id])
-    if @user == current_user
-    else
-      redirect_to user_path(current_user)
-    end
+    redirect_to user_path(current_user) unless @user == current_user
   end
   
   def update
