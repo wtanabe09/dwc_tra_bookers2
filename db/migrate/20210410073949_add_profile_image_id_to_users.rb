ba8d540e9c87cf1c40c4556b222dfceb80af355a
EC2 Default User <ec2-user@ip-172-31-28-159.ec2.internal>
2022-02-07 03:07:25 +0000

    ActiveStorage導入

--- db/migrate/20210410073949_add_profile_image_id_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddProfileImageIdToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :profile_image_id, :string
-    add_column :users, :introduction, :text
-  end
-end
